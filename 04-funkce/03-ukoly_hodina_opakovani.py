9475751c35bf549472265b50806bc3d47359bef8
RYGLOVA Aneta <anetryglova@seznam.cz>
2023-11-02 10:08:45 +0100

    reseni 04

--- 04-funkce/03-ukoly_hodina_opakovani.py
+++ 04-funkce/03-ukoly_hodina_opakovani.py
@@ -1,13 +1,49 @@
 #Úkol 1
 """Napište program, který vezme seznam slov a vytvoří nový seznam, který obsahuje délky těchto slov."""
 
+seznam_slov = ["pes", "kočka", "hory", "ryba"]
+delky_slov = []
+
+for slovo in seznam_slov:
+    delky_slov.append(len(slovo))
+
+print(delky_slov)
+
 
 #Úkol 2
 """Napište program, který vypočítá sinus každého úhlu v seznamu a uloží výsledky do nového seznamu. Použijte metodu sin z knihovny math."""
 
+import math
+
+uhly = [0, math.pi/4, math.pi/2, 3*math.pi/4, math.pi]
+sinusy = []
+
+for uhel in uhly:
+    sinusy.append(math.sin(uhel))
+
+print(sinusy)
+
 
 #Úkol 3
 """Napište program, který generuje 10 náhodných čísel a poté vytvoří seznam těch čísel, která jsou menší než 0.5."""
+
+
+import random
+
+num_count = 10
+
+random_numbers = []
+for _ in range(num_count):
+    random_numbers.append(random.random())
+
+filtered_numbers = []
+for num in random_numbers:
+    if num < 0.5:
+        filtered_numbers.append(num)
+
+print("Vygenerovaná náhodná čísla:", random_numbers)
+print("Čísla menší než 0.5:", filtered_numbers)
+
 
 
 #Úkol 4
@@ -29,7 +65,18 @@ school_report = {
     "Chemie": 4,
 }
 
+total_marks = sum(school_report.values())
+count = len(school_report)
+average_mark = total_marks / count
 
+print(f"Průměrná známka: {average_mark}")
+
+subjects_with_grade_1 = []
+for subject, grade in school_report.items():
+    if grade == 1:
+        subjects_with_grade_1.append(subject)
+
+print("Předměty s hodnocením 1:", subjects_with_grade_1)
 
 #Úkol 5
 """V následujícím slovníků je evidence automobilů. Klíčem jsou státní poznávací značky (SPZ) a hodnotou je jméno majitele vozu. Napiš program, který vypíše všechny majitele, jejichž vozidlo je registrováno v Plzeňském kraji, tj. na druhém místě (index 1!) řetězce v klíči je písmeno P."""
@@ -41,6 +88,10 @@ plates = {"4A2 3000": "František Novák",
     "37E 1252": "Martina Matušková",
     "2A5 2241": "Jan Král"}
 
+owners_in_Plzen = []
 
+for plate, owner in plates.items():
+    if plate[1] == 'P':
+        owners_in_Plzen.append(owner)
 
-
+print(f"Majitelé vozidel registrovaných v Plzeňském kraji:{owners_in_Plzen}")
--- 04-funkce/04-ukoly_hodina.py
+++ 04-funkce/04-ukoly_hodina.py
@@ -1,9 +1,22 @@
 #Úkol 1
 """Napiš funkci mult, která bude mít dva číselné parametry. Funkce oba parametry vynásobí a vrátí výsledek."""
 
+def mult(a, b):
+    return a * b
+
+vysledek = mult(5, 3)
+print(vysledek)
+
 
 #Úkol 2
 """Napište funkci vypocet_obvodu_obdelnika, která bude mít dva parametry (délku a šířku) a vrátí obvod obdélníka. o = 2(a+b)"""
+
+def vypocet_obvodu_obdelnika(delka, sirka):
+    obvod = 2 * (delka + sirka)
+    return obvod
+
+obvod = vypocet_obvodu_obdelnika(4, 6)
+print(obvod)
 
 
 #Úkol 3
@@ -15,19 +28,64 @@ Zadej slovo: ahoj
 ********
 """
 
+def obal_hvezdickami(retezec):
+    delka = len(retezec) + 4
+    print('*' * delka)
+    print(f'* {retezec} *')
+    print('*' * delka)
+
+obal_hvezdickami("ahoj")
+
 
 #Úkol 4
 """Napiš funkci month_of_birth, která bude mít jeden parametr - rodné číslo. Funkce ze zadaného rodného čísla určí měsíc narození, které vrátí jako výstup. Nezapomeň, že pro ženy je k měsíci připočtena hodnota 50."""
+
+def month_of_birth(rodne_cislo):
+    mesic_cislo = int(rodne_cislo[2:4])
+    
+    if mesic_cislo > 50:
+        mesic_cislo -= 50
+    
+    return mesic_cislo
+
+rodne_cislo_muz = "950212/1234"
+rodne_cislo_zena = "015312/5678"
+
+mesic_muz = month_of_birth(rodne_cislo_muz)
+mesic_zena = month_of_birth(rodne_cislo_zena)
+
+print(f"Měsíc narození muže: {mesic_muz}")
+print(f"Měsíc narození ženy: {mesic_zena}")
 
 
 #Úkol 5
 """Napište funkci analyza_textu, která bude mít jeden parametr (textový řetězec). Funkce spočítá a vrátí počet slov a počet vět v zadaném textu."""
 
+def analyza_textu(text):
+    interpunkce = [".", "!", "?"]
+    
+    slova = text.split()
+    pocet_slov = len(slova)
+    
+    pocet_vet = 0
+    for znak in interpunkce:
+        pocet_vet += text.count(znak)
+
+    return pocet_slov, pocet_vet
+
+print(analyza_textu("Ahoj, neco neco. Ahoj ahoj!"))
 
 #Úkol 6
 """Napište funkci palindrom, která bude mít jako parametr řetězec a vrátí True, pokud je řetězec palindrom, jinak vrátí False. Palindrom je řetězec, který je stejný i po převrácení."""
 
+def palindrom(retezec):
+    retezec_reverz = ""
+    for i in range(len(retezec) - 1, -1, -1):
+        retezec_reverz += retezec[i]
+    return retezec == retezec_reverz
 
-
+# Testování funkce
+print(palindrom("radar"))
+print(palindrom("hello"))
 
 
