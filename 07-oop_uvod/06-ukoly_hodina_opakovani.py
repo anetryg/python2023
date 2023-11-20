25a0564a8e62ee76964318e5acc856d84461c382
RYGLOVA Aneta <anetryglova@seznam.cz>
2023-11-20 12:53:48 +0100

    cv7

---
+++ 07-oop_uvod/06-ukoly_hodina_opakovani.py
@@ -0,0 +1,19 @@
+#Úkol 1
+"""Napište program, který bude přijímat vstup od uživatele a ukládat ho do textového souboru. Program by měl ošetřit situaci, kdy dojde k chybě při zápisu do souboru (IOError)."""
+
+
+
+#Úkol 2
+"""Vytvořte program, který načte obsah existujícího textového souboru a zobrazí prvních 10 řádků. Ošetřte možné chyby, například pokud soubor neexistuje."""
+
+
+
+#Úkol 3
+"""Napište program, který umožní uživateli zadat název existujícího textového souboru a poté vypíše, zda je soubor prázdný nebo ne. """
+
+
+
+
+#Úkol 4
+"""Vytvořte program, který umožní uživateli přidávat další řádky do existujícího textového souboru. Program by měl nejprve načíst obsah souboru a poté umožnit uživateli přidávat nové řádky."""
+
