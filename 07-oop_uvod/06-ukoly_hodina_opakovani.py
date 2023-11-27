2123270848040fb56a706a3cf6550dd7956df6a2
RYGLOVA Aneta <anetryglova@seznam.cz>
2023-11-27 13:32:31 +0100

    cv8

--- 07-oop_uvod/06-ukoly_hodina_opakovani.py
+++ 07-oop_uvod/06-ukoly_hodina_opakovani.py
@@ -1,11 +1,33 @@
 #Úkol 1
 """Napište program, který bude přijímat vstup od uživatele a ukládat ho do textového souboru. Program by měl ošetřit situaci, kdy dojde k chybě při zápisu do souboru (IOError)."""
 
+try:
+    with open("ukol_1.txt", "w") as file:
+        poznamka = input("Napiš něco: ")
+        file.write(poznamka)
+except IOError:
+    print("špatně")
+
 #Úkol 2
-"""Vytvořte program, který načte obsah existujícího textového souboru a zobrazí prvních 10 řádků. Ošetřte možné chyby, například pokud soubor neexistuje."""
+"""Napište program, který umožní uživateli zadat název existujícího textového souboru a poté vypíše, zda je soubor prázdný nebo ne (FileNotFoundError). """
+
+try:
+    with open("ukol_1.txt", "r") as file:
+        if file.read() == "":
+            print("Prázdný soubor.")
+        else:
+            print("Soubor není prázdný.")
+except FileNotFoundError:
+    print("soubor neexistuje")
 
 #Úkol 3
-"""Napište program, který umožní uživateli zadat název existujícího textového souboru a poté vypíše, zda je soubor prázdný nebo ne. """
-
-#Úkol 4
 """Vytvořte program, který umožní uživateli přidávat další řádky do existujícího textového souboru. Program by měl nejprve načíst obsah souboru a poté umožnit uživateli přidávat nové řádky."""
+
+file_name = input("Zadej název souboru: ")
+
+with open(file_name,"a+") as file:
+    file.seek(0)  # Přesun kurzoru na začátek souboru
+    print(file.read())
+    file.write(input("Zadej text: "))
+    
+    
