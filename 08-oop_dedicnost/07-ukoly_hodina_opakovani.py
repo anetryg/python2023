ecdf55ef7b056ce5778f6d21b1fcfc0c5b96566d
RYGLOVA Aneta <anetryglova@seznam.cz>
2023-12-04 16:30:38 +0100

    cv9

--- 08-oop_dedicnost/07-ukoly_hodina_opakovani.py
+++ 08-oop_dedicnost/07-ukoly_hodina_opakovani.py
@@ -2,11 +2,42 @@
 """ Vytvořte třídu Car, která reprezentuje automobil. Třída by měla mít atributy brand (značka auta), model (model auta), a is_running (informace o tom, zda je auto zapnuté - True, pokud ano, False, pokud ne). Přidej metodu start_engine(), která zapne motor auta, pokud není již zapnutý.
 """
 
+class Car:
+    """ něco něco """
+    def __init__(self, brand: str, model: str, is_running: bool):
+        self.brand = brand
+        self.model = model
+        self.is_running = is_running
+    
+    def start_engine(self):
+        if self.is_running == False:
+            self.is_running = True
+            print("Nastartoval jsi")
+        else:
+            print("motor už běží")
+
+auto = Car("vw", "golf", False)
+auto.start_engine()
 
 # Úkol 2
 """ Navrhněte třídu Movie reprezentující film. Třída by měla mít atributy title (název filmu), director (režisér), a duration (délka filmu v minutách). Přidej metodu get_full_info(), která vypíše kompletní informace o filmu. Pokud je délka filmu neznámá (None), vypište pouze název a režiséra.
 """
-
+class Movie:
+    def __init__(self,title,director,durtion):
+        self.title = title
+        self.director = director
+        self.duration = durtion
+        
+    def get_full_info(self):
+        if self.duration == None:
+            print(self.director, self.title)
+            
+        else:
+            print(self.title, self.director, self.duration)
+            
+film = Movie("Pán prstenů", "J:R:R. Tolkien", None)
+film.get_full_info()
+            
 
 # Úkol 3
 """ Vytvoř třídu Song reprezentující píseň. Třída by měla mít atributy title (název písně), artist (interpret), duration (délka písně v minutách), a genre (žánr písně, defaultně nastaven na "Pop"). Přidej metodu play(), která vypíše zprávu o přehrávání písně. Před přehráním písně ověřte, zda je délka písně větší než 0, a vypište odpovídající zprávu.
@@ -16,4 +47,20 @@
 # Úkol 4
 """ Vytvořte třídu Dog reprezentující psa. Třída by měla mít atributy name (jméno psa), breed (plemeno psa), age (věk psa), a is_hungry (informace o tom, zda je pes hladový - True, pokud ano, False, pokud ne). Přidej metodu feed(), která nakrmí psa a změní stav is_hungry na False. Pokud je pes již nakrmený, vypište o tom informaci.
 """
+class Dog:
+    def __init__(self,name,breed,age,is_hungry):
+        self.name=name
+        self.breed=breed
+        self.age=age
+        self.is_hungry=is_hungry
+    def feed(self):
+        if self.is_hungry==False:
+            self.is_hungry=True
+            print('pes je nakrmen')
+        else:
+            print('pes není hladový')
+pes=Dog('Alík','pudl','10',False)
+pes.feed()
+pes.feed()
+print(pes.name, pes.age)
 
--- 08-oop_dedicnost/08-ukoly_hodina.py
+++ 08-oop_dedicnost/08-ukoly_hodina.py
@@ -1,3 +1,5 @@
+import math
+
 # Úkol 1
 """ Uvažujme, že chceme modelovat různé geometrické tvary jako čtverec, kruh a obdélník. Využijeme dědičnosti k vytvoření obecné třídy Shape.
  
@@ -11,6 +13,19 @@ Třída Shape by měla mít následující metody:
 - calculate_area(self): Metoda pro výpočet obsahu tvaru (metoda s pass, určená k přepsání v odvozených třídách).
 """
     
+class Shape:
+    def __init__(self, color="white", filled=True):
+        self.color = color
+        self.filled = filled
+        
+    def display_info(self):
+        print(self.color, self.filled)
+        
+    def calculate_area(self):
+        pass
+
+
+
 
 # Úkol 2
 """ Od třídy Shape odvoďtě třídy Rectangle a Circle.
@@ -24,6 +39,27 @@ Třída Circle by měla mít následující atributy:
 
 Obě třídy by měly přepsat metodu calculate_area pro konkrétní výpočet obsahu pro každý tvar.
 """
+class Rectangle(Shape):
+    def __init__(self, length, width, color="white", filled=True):
+        super().__init__(color, filled)
+        self.length = length
+        self.width = width
+        
+    def calculate_area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius, color="white", filled=True):
+        super().__init__(color, filled)
+        self.radius = radius
+        
+    def calculate_area(self):
+        return (math.pi * self.radius)**2
+    
+""" shape1 = Circle(5)
+print(shape1.calculate_area()) 
+shape2 = Rectangle(2, 6)
+print(shape2.calculate_area()) """
 
 
 # Úkol 3
@@ -40,7 +76,29 @@ Třída ClothingItem by měla mít následující metody:
 - display_info(self): Metoda pro zobrazení informací o kusu oblečení.
 - calculate_discounted_price(self, discount_percentage): Metoda pro výpočet slevy na cenu oblečení."""
 
+class ClothingItem:
+    def __init__(self,name, size, color, prize):
+        self.name = name
+        self.size = size
+        self.color = color
+        self.prize = prize
+        
+    def display_info(self):
+        print(self.name, self.size, self.color, self.prize)
+        
+    def calculate_discounted_price(self, procento_slevy):
+        discount = (procento_slevy / 100) * self.prize
+        discounted_price = self.prize - discount
+        return discounted_price
+    
+""" tricko = ClothingItem("polo", "XL", "modra", 1000)
 
+tricko.display_info()
+
+print(tricko.calculate_discounted_price(15)) """
+
+
+    
 # Úkol 4
 """ Od třídy ClothingItem odvoďtě třídy TShirt a Pants.
 
@@ -58,8 +116,44 @@ Metody pro třídu Pants:
 
 """
 
+class TShirt(ClothingItem):
+    def __init__(self,name, size, color, prize, fabric):
+        super().__init__(name, size, color, prize)
+        self.fabric = fabric
+    def add_logo(self, logo):
+        print(f"{logo}Polo bolo pridané")
+        
+class Pants(ClothingItem):
+    def __init__(self,name, size, color, prize, fit):
+        super().__init__(name, size, color, prize)
+        self.fit = fit
+    def adjust_length(self, new_length):
+        print(f"Dlžka bola zmenená")
+        
+tricko = TShirt("Nike", "XL", "Black", 45, "bavlna")
+kalhoty = Pants("Adidas", "L", "Blue", 50, "Slim Fit")
+
+tricko.add_logo("Nike")
+print(tricko.calculate_discounted_price(30))
+kalhoty.adjust_length("120")
+        
 
 # Úkol 5
 """ Přepiš předchozí příklad pomocí datových tříd."""
 
 from dataclasses import dataclass
+
+@dataclass
+class ClothingItem:
+    name: str
+    size: str
+    color: str
+    prize: int
+        
+    def display_info(self):
+        print(self.name, self.size, self.color, self.prize)
+        
+    def calculate_discounted_price(self, procento_slevy):
+        discount = (procento_slevy / 100) * self.prize
+        discounted_price = self.prize - discount
+        return discounted_price
